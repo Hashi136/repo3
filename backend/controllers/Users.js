40887df97a760fcffbb8f0f537e1306bbfbe6b19
Luminda-lakmal <luckmald98@gmail.com>
2022-06-21 00:35:24 +0530

    Added email validation

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -17,6 +17,14 @@ export const Register = async(req, res) => {
     const { name, email, password, confPassword } = req.body;
     if(password != confPassword) return res.status(400).json({msg: "Password and Confirm Password do not match"});
     if(name.length >= 15) return res.status(400).json({msg: "User name can't be more than 15 characters"});
+
+    function validateEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    if(!validateEmail(email)) return res.status(400).json({msg: "enter a valid email address"});
+    
     const salt = await bcrypt.genSalt();
     const hashPassword = await bcrypt.hash(password, salt);
     try {
